8d72870d86ffeac154db6c122dbd7fcb64e00c4c
aarohanverma <97247457+aarohanverma@users.noreply.github.com>
2022-02-03 20:44:37 +0530

    Update SpirallyTraversingAMatrix.cpp

--- Microsoft/Question 04/SpirallyTraversingAMatrix.cpp
+++ Microsoft/Question 04/SpirallyTraversingAMatrix.cpp
@@ -1,65 +1,83 @@
 // { Driver Code Starts
-//Initial Template for C
-
-
-
-#include<stdio.h>
+#include <bits/stdc++.h> 
+using namespace std; 
 
  // } Driver Code Ends
-//User function Template for C
-
-
-void rotate(int n,int a[][n])
-{
-    int temp;
-    for(int i=0;i<n;i++)
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
     {
-        for(int j=0;j<n;j++)
-        {
-            if(j>i)
-            {
-                temp=a[i][j];
-                a[i][j]=a[j][i];
-                a[j][i]=temp;
-            }
-        }
-    }
-    for(int i=0;i<n/2;i++)
+    int lt=0;
+    int rt=c;
+    int up=0;
+    int dn=r;
+    vector<int> v;
+    while(up<dn && lt<rt)
     {
-        for(int j=0;j<n;j++)
+        int i=lt;
+        int j=up;
+        for(;i<rt;i++)
         {
-            temp=a[i][j];
-            a[i][j]=a[n-i-1][j];
-            a[n-i-1][j]=temp;
+            v.push_back(matrix[j][i]);
         }
+        rt--;
+        i=rt;
+        j=up+1;
+        
+        for(;j<dn;j++)
+        {
+            v.push_back(matrix[j][i]);
+        }
+        dn--;
+        i=rt-1;
+        j=dn;
+        
+        for(;i>=lt && j>up;i--)
+        {
+            v.push_back(matrix[j][i]);
+        }
+        i=lt;
+        j=dn-1;
+        
+        for(;j>up && i<rt;j--)
+        {
+            v.push_back(matrix[j][i]);
+        }
+        up++;
+        lt++;
+           
     }
-}
+    return v; 
+    }
+};
 
 // { Driver Code Starts.
-
-int main()
-{
+int main() {
     int t;
-    scanf("%d",&t); 
-    while(t--)
+    cin>>t;
+    
+    while(t--) 
     {
-        int n;
-        scanf("%d",&n);
-        int a[n][n];
-        for(int i=0; i<n; i++)
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
         {
-            for(int j=0; j<n; j++)
-                scanf("%d",&a[i][j]);
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
         }
-        rotate(n,a);
-        for (int i = 0; i < n; ++i)
-        {
-            for(int j=0; j<n; j++)
-                printf("%d ",a[i][j]);
-            printf("\n");
-        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
     }
     return 0;
-}
-
-  // } Driver Code Ends
+}  // } Driver Code Ends
